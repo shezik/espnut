9f0c38c4babf8af65df2fd906ff71709e2f31507
Kerry Shen <47771243+shezik@users.noreply.github.com>
2023-02-08 13:18:09 +0800

    Update PowerMgr.h

--- include/PowerMgr.h
+++ include/PowerMgr.h
@@ -18,30 +18,37 @@ class PowerMgr {
         uint32_t deepSleepTimeout;   // In ms
         int64_t nextDeepSleep;       // In ms
         uint32_t frequency;
-        bool woken = false;
+        bool wokenUp = false;
         void (*deepSleepCallback)() = nullptr;
         static PowerMgr *context;
     public:
         PowerMgr(KeyboardMgr &, uint8_t, uint8_t, uint8_t);
         ~PowerMgr();
+
         bool enterModemSleep();
         void enterDeepSleep();
         bool wokenUpFromDeepSleep();
         void init();
         void tick();
+
         bool getDisplayPower();
         void setDisplayPower(bool);
+
         bool getBacklightPower();
         void setBacklightPower(bool);
+
         uint16_t getBacklightTimeout();
         void setBacklightTimeout(uint32_t);
         void feedBacklightTimeout();
+
         uint32_t getDeepSleepTimeout();
         void setDeepSleepTimeout(uint32_t);
         void feedDeepSleepTimeout();
+
         bool setFrequency(uint32_t);
         bool reduceFrequency();
         bool restoreFrequency();
+        
         void registerDeepSleepCallback(void (*)());
         static void keyPressCallback();
 };
