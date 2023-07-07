12645e5ed88dabb34e294442c59aac0cc73bf9fc
Kerry Shen <47771243+shezik@users.noreply.github.com>
2023-07-07 10:47:37 +0800

    Reduce keyboard scanning interval
    wow responsive

--- include/Configuration.h
+++ include/Configuration.h
@@ -52,7 +52,7 @@
 #define XBM_FONT_FILENAME "xbmFont128x32.h"
 
 // KeyboardMgr
-#define KEY_SCAN_INTERVAL 100  // ms
+#define KEY_SCAN_INTERVAL 10  // ms
 #define KEY_QUEUE_LENGTH 32
 #define ROW_GPIOS 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
 #define COL_GPIOS 14, 15, 16, 17
