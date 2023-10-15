d021e734412620827c5430f66e7d8ba581bd5c13
Kerry Shen <47771243+shezik@users.noreply.github.com>
2023-10-15 14:25:41 +0800

    Create copy-firmware-images.bat

---
+++ copy-firmware-images.bat
@@ -0,0 +1,5 @@
+copy .pio\build\esp32doit-devkit-v1\littlefs.bin .
+copy .pio\build\esp32doit-devkit-v1\bootloader.bin .
+copy .pio\build\esp32doit-devkit-v1\partitions.bin .
+copy .pio\build\esp32doit-devkit-v1\firmware.bin .
+copy %USERPROFILE%\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin .
